a2fb6fd5eb775b7292cdef605dcd75d0f4039317
milan <milan.correggio@gmail.com>
2023-01-02 11:58:02 +0900

    Fix(golang): Handle greetings error

--- golang/main.go
+++ golang/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
-	"golang/greetings"
+	"log"
 
-	"rsc.io/quote"
+	"golang/greetings"
 )
 
 func main() {
-	fmt.Println(greetings.Hello("me"))
-	fmt.Println(quote.Go())
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
 }
