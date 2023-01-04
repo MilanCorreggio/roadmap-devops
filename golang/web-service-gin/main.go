9d8873bbd77b603f3c3adee595e9053bc8b9c93b
milan <milan.correggio@gmail.com>
2023-01-04 21:21:47 +0900

    Add(golang): add get by ID album

--- golang/web-service-gin/main.go
+++ golang/web-service-gin/main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/albums", handler.GetAlbums)
+	router.GET("/albums/:id", handler.GetAlbumByID)
+
 	router.POST("/albums", handler.PostAlbums)
 
 	router.Run("localhost:8080")
